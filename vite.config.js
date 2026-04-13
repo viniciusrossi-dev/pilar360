8aaf39fd7f74a880236486839c97705378f58065
viniciusrossi-dev <viniciusrossi@hotmail.com.br>
2026-04-12 21:28:12 -0300

    Vini | fix:build target esnext para suportar top-level await

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   build: {
+    target: 'esnext',
     rollupOptions: {
       input: {
         index: resolve(__dirname, 'index.html'),
